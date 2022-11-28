08758764ebebf38a348125c6682d38bc53df3299
zwwuu <98434813+zwwuu@users.noreply.github.com>
2022-11-28 04:12:52 -0500

    termux

--- src/components/UI/Card/Card.js
+++ src/components/UI/Card/Card.js
@@ -1,12 +1,15 @@
 import styles from './Card.module.css';
 import cn from 'clsx';
+import {motion} from 'framer-motion'
 
 export default function Card({ header, body, footer, className }) {
   return (
-    <div className={cn(styles.root, className)}>
+    <motion.div whileTap={{ y: 1 }}
+    whileHover={{ y: 1 }}
+    className={cn(styles.root, className)}>
       <div className={styles.header}>{header}</div>
       <div className={styles.body}>{body}</div>
       {footer && <div className={styles.footer}>{footer}</div>}
-    </div>
+    </motion.div>
   );
 }
